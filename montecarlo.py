da69d5438787affce3e6c9407142890f252d1d26
raycakraa <50711922+raycakraa@users.noreply.github.com>
2021-03-10 23:29:22 +0700

    Update montecarlo.py

--- montecarlo.py
+++ montecarlo.py
@@ -53,7 +53,6 @@ if uploaded_file is not None:
         reslist.append(res)
     st.sidebar.text("")
 
-    # st.text(reslist[1])
     aa = 0
     ab = 0
     for i in range(len(y)):
@@ -61,5 +60,50 @@ if uploaded_file is not None:
         st.sidebar.text("BB Interval "+str(i)+": "+str(round(ab, 5)))
         ab = aa + 0.001
 
+    des = 229900
+    st.text("Harga Tableware: Rp."+str(rupiah_format(des)))
+    col1, col2, col3 = st.beta_columns(3)
+    randomlist = []
+    for i in range(1, 16):
+        n = random.random()
+        col1.text("Angka acak "+str(i)+": "+str(round(n, 3)))
+        randomlist.append(n)
+
+    a = 1
+    perlist = []
+    for i in range(len(randomlist)):
+        if (randomlist[i] <= reslist[0]):
+            b = 0
+            col2.text("Permintaan "+str(a)+": "+str(b))
+        elif (randomlist[i] <= reslist[1]):
+            b = 1
+            col2.text("Permintaan "+str(a)+": "+str(b))
+        elif (randomlist[i] <= reslist[2]):
+            b = 2
+            col2.text("Permintaan "+str(a)+": "+str(b))
+        elif (randomlist[i] <= reslist[3]):
+            b = 3
+            col2.text("Permintaan "+str(a)+": "+str(b))
+        elif (randomlist[i] <= reslist[4]):
+            b = 4
+            col2.text("Permintaan "+str(a)+": "+str(b))
+        perlist.append(b)
+        a = a+1
+
+    c = 1
+    pemlist = []
+    for i in range(len(perlist)):
+        d = perlist[i]*int(des)
+        col3.text("Pemasukan "+str(c)+": Rp."+str(rupiah_format(d)))
+        pemlist.append(d)
+        c = c+1
+
+    per = int(find_sum(perlist))
+    st.text("")
+    st.text("Jumlah Permintaan : "+str(per))
+    aver_per = int(find_sum(perlist))/len(perlist)
+    st.text("Rata-rata Permintaan Per Hari: "+str(aver_per))
+    aver_pem = int(find_sum(pemlist))/len(pemlist)
+    st.text("Rata-rata Pemasukan Per Hari: Rp."+str(rupiah_format(aver_pem)))
 else:
     st.write("Silahkan Pilih file csv yang akan digunakan !")
